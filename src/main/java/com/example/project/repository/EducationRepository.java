fd8331621387e9a3d956edd2eed22a26e1688ce0
oscarjundt <oscar.jundt-schmitter@laposte.net>
2025-12-15 00:51:59 +0100

    feat(repo): annotate EducationRepository with @Repository

--- src/main/java/com/example/project/repository/EducationRepository.java
+++ src/main/java/com/example/project/repository/EducationRepository.java
@@ -2,6 +2,8 @@ package com.example.project.repository;
 
 import com.example.project.entity.Education;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EducationRepository extends CrudRepository<Education, Long> {
 }
